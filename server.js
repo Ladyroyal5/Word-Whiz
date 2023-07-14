9e2bc525aa8d421593408577ad68b1e4ee64112d
Ladyroyal5 <ladyroyal.ellalily@gmail.com>
2023-07-14 01:18:40 +0100

    message

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-
 app.get('/', function(request, response){
-    response.sendFile(path.join(__dirname, './html_files', 'landingpage.html'));
+    response.sendFile(path.join(__dirname, './public', 'landingpage.html'));
 });
 
-app.use(express.static('html_files'));
+app.use(express.static('public'));
 
 app.listen(5500, function(){
     console.log("server is up at port 5500");
